b694a7562230adeb179960968235e5288cbc232c
Francesc Valero <francesc.valero@estudiantat.upc.edu>
2023-03-12 11:24:48 +0100

    codigo definitivo

--- fvr_primos.py
+++ fvr_primos.py
@@ -29,7 +29,51 @@ def primos(numero):
     Devuelve una tupla con todos los números primos menores que su argumento.    
     """
 
-    return tuple([prova for prova in range(2, numero) if esPrimo(prova)])
+    return tuple([prova for prova in range(2, numero) if esPrimo(numero)])
+
+def descompon(numero):
+    """
+    Devuelve una tupla con la descomposición en factores primos de su argumento
+    """
+    
+    return tuple([i for i in primos(numero) if numero % i ==0])
+
+
+def mcm(numero, numero2):
+    """
+    Devuelve el mínimo común múltiplo de sus argumentos.
+    """
+    numeroD = dict(descompon(numero))
+    numero2D = dict(descompon(numero2))
+
+
+    factoritzacion = numeroD | numero2D  
+
+    mcm = 1 
+    for factor, exp in factoritzacion.items():
+        mcm *= (factor**exp)
+
+    return mcm
+
+
+def mcd(numero, numero2):
+    """
+    Devuelve el máximo común divisor de sus argumentos.
+    """
+    numeroD = dict(descompon(numero))
+    numero2D = dict(descompon(numero2))
+
+
+    factoritzacion = numeroD & numero2D  
+
+    mcd = 1 
+    for factor, exp in factoritzacion.items():
+        mcd *= (factor**exp)
+
+    return mcd
+
 
 import doctest
 doctest.testmod()
+
+
