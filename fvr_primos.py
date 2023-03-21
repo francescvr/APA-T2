d3a482e0644bf93c2c54830202f0f03d8cf2b05c
Francesc Valero <francesc.valero@estudiantat.upc.edu>
2023-03-21 15:28:12 +0100

    codigo

--- fvr_primos.py
+++ fvr_primos.py
@@ -44,10 +44,10 @@ def descompon(numero):
     
     return tuple(factores)
 
-from collections import Counter as C  
+#from collections import Counter as C  
     
 
-def mcm(numero, numero2):
+#def mcm(numero, numero2):
     """
     Devuelve el mínimo común múltiplo de sus argumentos.
     >>> mcm(24, 4)
@@ -66,7 +66,7 @@ def mcm(numero, numero2):
     return mcm
 
 
-def mcd(numero, numero2):
+#def mcd(numero, numero2):
     """
     Devuelve el máximo común divisor de sus argumentos.
     >>> mcd(24, 4)
@@ -83,6 +83,127 @@ def mcd(numero, numero2):
         mcd *= (factor**exp)
 
     return mcd
+
+
+## mcm y mcd sin usar la librería collections
+
+def fact(numero1, numero2):
+        """
+        Devuelve 2 diccionarios con la descomposición en factores de los dos números que se usan omo argumentos de la función
+        """
+
+        factores1 = descompon(numero1)
+        factores2 = descompon(numero2)
+
+        factores = set(factores1) | set(factores2)
+        
+
+        dic1 = {factor : 0 for factor in factores}
+        dic2 = {factor : 0 for factor in factores}
+
+
+        for factor in factores1:
+            dic1[factor] += 1
+
+        for factor in factores2:
+            dic2[factor]+= 1
+
+
+        return dic1, dic2
+
+
+
+def mcm(numero1, numero2):
+    """
+    Devuelve el mínimo común múltiplo de sus argumentos.
+    >>> mcm(90, 14)
+    630
+    """
+
+    dic1,dic2 = fact(numero1,numero2)
+
+    mcm = 1
+
+    for factor in dic1:
+        mcm = mcm * factor**max(dic1[factor],dic2[factor])
+
+    return mcm
+
+
+def mcd(numero1, numero2):
+    """
+    Devuelve el máximo común divisor de sus argumentos.
+    >>> mcd(924, 780)
+    12
+    """
+
+    dic1, dic2 = fact(numero1, numero2)
+
+    mcd = 1
+
+    for factor in  dic1 | dic2:
+        mcd = mcd * factor ** min(dic1[factor],dic2[factor])
+
+    return mcd 
+
+
+## mcm y mcdc para un número arbitrario de argumentos
+
+def fact(*numeros):
+    """
+    Devuelve un diccionario con la descomposición en factores de cada número que se pasa como argumento.
+    """
+
+    factores = set()
+    for numero in numeros:
+        factores |= set(descompon(numero))
+
+    diccionarios = []
+    for numero in numeros:
+        diccionario = {factor: 0 for factor in factores}
+        factores_numero = descompon(numero)
+        for factor in factores_numero:
+            diccionario[factor] += 1
+        diccionarios.append(diccionario)
+
+    return diccionarios
+
+
+def mcm(*numeros):
+    """
+    Devuelve el mínimo común múltiplo de los argumentos.
+    >>> mcm(42, 60, 70, 63)
+    1260
+    """
+
+    diccionarios = fact(*numeros)
+
+    mcm = 1
+    for factor in diccionarios[0]:
+        max_potencia = max(diccionario[factor] for diccionario in diccionarios)
+        mcm *= factor ** max_potencia
+
+    return mcm
+
+
+def mcd(*numeros):
+    """
+    Devuelve el máximo común divisor de los argumentos.
+    >>> mcd(840, 630, 1050, 1470)
+    210
+    """
+
+    diccionarios = fact(*numeros)
+
+    mcd = 1
+    for factor in diccionarios[0]:
+        min_potencia = min(diccionario[factor] for diccionario in diccionarios)
+        mcd *= factor ** min_potencia
+
+    return mcd
+
+
+
 
 
 import doctest
